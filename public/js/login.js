50655be555db774e39368e42449dcd3ca3007ba4
syifaliaa <amaliasyifaazzahro04@gmail.com>
2026-06-24 10:00:03 +0700

    Update login.js

--- public/js/login.js
+++ public/js/login.js
@@ -1,5 +1,4 @@
 document.getElementById("loginForm").addEventListener("submit", async (e) => {
-
     e.preventDefault();
 
     const data = Object.fromEntries(
@@ -7,38 +6,27 @@ document.getElementById("loginForm").addEventListener("submit", async (e) => {
     );
 
     try {
-
         const res = await fetch("/api/login", {
             method: "POST",
-
             headers: {
                 "Content-Type": "application/json"
             },
-
             body: JSON.stringify(data)
         });
 
         const result = await res.json();
 
         if (result.success) {
-
+            localStorage.setItem("isAdmin", "true"); // ← tambah ini
             document.getElementById("msg").innerText =
                 "Login berhasil...";
-
             window.location.href = "/admin";
-
         } else {
-
             document.getElementById("msg").innerText =
                 "Username atau password salah!";
-
         }
-
     } catch (err) {
-
         document.getElementById("msg").innerText =
             "Terjadi kesalahan.";
-
     }
-
 });
